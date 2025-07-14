65d815d698fda4b62f05b40968506575033f2230
keon22han <keonheehan@hansung.ac.kr>
2025-07-15 01:38:11 +0900

    ✨ Feature: #18 게임 내에서 사용되는 확정 대사들을 ConstantGameDialogs 로 분리
    게임 내에서 사용되는 확정 대사들을 ConstantGameDialogs 로 분리하였습니다.

---
+++ NLP/NLP/Sources/Utils/Constants/ConstantGameDialogs.swift
@@ -0,0 +1,20 @@
+//
+//  ConstantGameDialogs.swift
+//  NLP
+//
+//  Created by Ted on 7/15/25.
+//
+
+struct ConstantGameDialogs {
+    static let introDialogs: [String] = [
+        "…우우우우우웅…",
+        "귀 안쪽에서 울리는 무언의 진동음. 정적 위에 얹힌 소음은 마치 몸속 심장 박동처럼 느껴졌다.",
+        "눈을 떴다.",
+        "오래된 배선들과 부서진 격벽, 그리고 어둠 속에서 깜빡이는 단 하나의 붉은 불빛.",
+        "나는 바닥에 누워 있다. 아니지, 공중을 부유하고 있다.",
+        "숨을 쉬자 폐 안쪽이 타들어가는 듯한 고통이 밀려왔다.",
+        "산소가 부족하다."
+    ]
+    
+    static let introDialogBeforeStageOneStart: String = "나는...\n이곳에서 살아남을 수 있을까?"
+}
