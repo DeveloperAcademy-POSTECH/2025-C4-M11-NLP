a79702b6649669fc4ef99ff1160ae56daa9cb693
keon22han <keonheehan@hansung.ac.kr>
2025-07-14 15:54:39 +0900

    ✨ Feature: #14 RootView(게임의 시작점), RootViewModel 틀 구현
    RootView(게임의 시작점), RootViewModel 틀을 구현하였습니다.

---
+++ NLP/NLP/Sources/Presentation/RootScene/RootViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  RootViewModel.swift
+//  NLP
+//
+//  Created by Ted on 7/14/25.
+//
+
+import SwiftUI
+
+class RootViewModel: ViewModelable {
+    struct State {
+        // TODO
+    }
+    
+    enum Action {
+        // TODO
+    }
+    
+    @Published var state: State = State()
+    
+    func action(_ action: Action) {
+        // TODO
+    }
+}
