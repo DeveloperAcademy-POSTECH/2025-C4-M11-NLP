a7746f96e2f7cd75669de5dc6e7c2625ad85b258
keon22han <keonheehan@hansung.ac.kr>
2025-07-14 15:47:02 +0900

    ✨ Feature: #14 App Lifecycle 관리를 위한 AppDelegate 구현
    App Lifecycle 관리를 위한 AppDelegate를 구현하였습니다.

---
+++ NLP/NLP/Sources/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  NLP
+//
+//  Created by Ted on 7/14/25.
+//
+
+import UIKit
+
+// MARK: - 앱 Lifecycle과 관련된 로직을 수행하는 클래스
+class AppDelegate: NSObject, UIApplicationDelegate {
+    // MARK: - 앱 시작 시 가장 먼저 호출되는 메서드로, 이전 사용자의 게임 진행 상태에 따른 게임 화면 구성 등의 로직을 작성하면 될 것 같습니다!
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
