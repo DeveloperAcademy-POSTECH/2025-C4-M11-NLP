d4fbf53b068730300d6c6b97f97d0553cc62cb21
wonCha25 <chawj1234@gmail.com>
2025-07-20 22:47:10 +0900

    design: DialogView UI 구현
    DialogView UI 구현했습니다

--- NLP/NLP/Sources/Common/DesignSystem/DialogView.swift
+++ NLP/NLP/Sources/Common/DesignSystem/DialogView.swift
@@ -17,28 +17,8 @@ struct DialogView: View {
     var body: some View {
         VStack {
             Spacer().frame(height: 50)
-            HStack {
-                Spacer()
-                Button(action: {
-                    isPresented = false
-                }) {
-                    Image(systemName: "x.square.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundStyle(.white)
-                        .frame(width: 30)
-                }
-                .buttonStyle(.plain)
-                Spacer().frame(width: 24)
-            }
             
-            Spacer()
-            
-            //StreamingText View로 텍스트 재활용
-            Rectangle()
-                .fill(.black.opacity(0.8))
-                .frame(width: ConstantScreenSize.screenWidth * 0.9, height: ConstantScreenSize.screenHeight * 0.3)
-                .border(Color.yellow, width: 7)
+            BackgroundView(isPresented: $isPresented)
                 .overlay(
                     ScrollView{
                         VStack(alignment: .leading, spacing: 5){
@@ -57,44 +37,114 @@ struct DialogView: View {
                             }
                         }
                         .frame(maxWidth: .infinity, alignment: .leading) // 추가 보장
+                        
+                        TextField("내용을 입력", text: $inputText)
+                            .font(NLPFont.body)
+                            .foregroundStyle(.red)
+                            .textFieldStyle(.plain) // 깔끔한 스타일 (원하면 수정)
+                            .submitLabel(.done)      // 키보드의 return 키에 "Done" 표시 (필요시 다른 옵션도 가능)
+                            .onSubmit {
+                                dialogManager.respond(inputText, dialogPartnerType: dialogManager.currentPartner ?? .computer, isLogged: true)
+                                inputText = ""       // 입력 후 초기화
+                            }
                     }
-                        .frame(
-                            width: ConstantScreenSize.screenWidth * 0.8,
-                            height: ConstantScreenSize.screenHeight * 0.25
-                        )
-                )
-
-            HStack {
-                TextField("", text: $inputText)
-                    .font(NLPFont.body)
-                    .padding(.horizontal, 12)
-                    .frame(height: 50)
-                    .background(Color.black)
-                    .foregroundStyle(.white)
-                    .overlay(
-                        Rectangle().stroke(Color.white, lineWidth: 3)
+                    .frame(
+                        width: ConstantScreenSize.screenWidth * 0.82,
+                        height: ConstantScreenSize.screenHeight * 0.4
                     )
-                Button {
-                    if !dialogManager.isGenerating {
-                        dialogManager.respond(inputText, dialogPartnerType: dialogManager.currentPartner ?? .computer, isLogged: true)
-                        inputText = ""
-                    }
-                    
-                } label: {
-                    Image(systemName: "arrowshape.up.fill")
-                        .font(.system(size: 24, weight: .bold))
-                        .foregroundStyle(.white)
-                        .frame(width: 50, height: 50)
-                        .background(Color.black)
-                        .overlay(
-                            Rectangle().stroke(Color.white, lineWidth: 3)
-                        )
-                }
-            }
-                .frame(width: ConstantScreenSize.screenWidth * 0.9, height: 50)
-                .padding(.bottom, 110) //
-            }
+                    .border(Color.green)
+
+                )
+           
+
+            
+
+//            HStack {
+//                TextField("", text: $inputText)
+//                    .font(NLPFont.body)
+//                    .padding(.horizontal, 12)
+//                    .frame(height: 50)
+//                    .background(Color.black)
+//                    .foregroundStyle(.white)
+//                    .overlay(
+//                        Rectangle().stroke(Color.white, lineWidth: 3)
+//                    )
+//                Button {
+//                    if !dialogManager.isGenerating {
+//                        dialogManager.respond(inputText, dialogPartnerType: dialogManager.currentPartner ?? .computer, isLogged: true)
+//                        inputText = ""
+//                    }
+//                    
+//                } label: {
+//                    Image(systemName: "arrowshape.up.fill")
+//                        .font(.system(size: 24, weight: .bold))
+//                        .foregroundStyle(.white)
+//                        .frame(width: 50, height: 50)
+//                        .background(Color.black)
+//                        .overlay(
+//                            Rectangle().stroke(Color.white, lineWidth: 3)
+//                        )
+//                }
+//            }
+//                .frame(width: ConstantScreenSize.screenWidth * 0.9, height: 50)
+//                .padding(.bottom, 110) //
         }
     }
+}
+
+
+struct BackgroundView: View {
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+                  
+        Rectangle()
+            .opacity(0.1)
+            .frame(width: ConstantScreenSize.screenWidth * 0.9, height: ConstantScreenSize.screenHeight * 0.5)
+            .border(Color.yellow, width: 1)
+            .overlay {
+                GeometryReader { geometry in
+                    ZStack {
+                        Button {
+                            isPresented = false
+                        } label: {
+                            Image("x-symbol")
+                                .resizable()
+                                .frame(width: 24, height:24)
+                                .position(x: geometry.size.width - 27, y: 27)
+                        }
+                
+                        Image("dialogView-LT")
+                            .resizable()
+                            .frame(width: 20, height:20)
+                            .position(x: 7, y: 7)
+                        
+                        Image("dialogView-RT")
+                            .resizable()
+                            .frame(width: 20, height:20)
+                            .position(x: geometry.size.width - 7, y: 7)
+                        
+                        Image("dialogView-LD")
+                            .resizable()
+                            .frame(width: 20, height:20)
+                            .position(x: 7, y: geometry.size.height - 7)
+                        
+                        Image("dialogView-RD")
+                            .resizable()
+                            .frame(width: 20, height:20)
+                            .position(x: geometry.size.width - 7, y: geometry.size.height - 7)
+                    }
+                }
+            }
+    }
+}
+
+
+#Preview {
+    DialogView(
+        dialogManager: DialogManager(),
+        isPresented: .constant(true)
+    )
+}
 
 
