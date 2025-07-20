f8417b322fe44bd9a4c4af94cb3c355189c915eb
keon22han <keonheehan@hansung.ac.kr>
2025-07-21 02:11:11 +0900

    refactor: DialogView 구조 개선
    DialogView의 구조를 개선하였습니다.

--- NLP/NLP/Sources/Common/DesignSystem/DialogView.swift
+++ NLP/NLP/Sources/Common/DesignSystem/DialogView.swift
@@ -20,63 +20,43 @@ struct DialogView: View {
 
     
     var body: some View {
-        VStack {
-            BackgroundView(isPresented: $isPresented)
-                .overlay(
-                    ScrollView {
-                        VStack(alignment: .leading, spacing: 0) {
-                            ForEach(Array(dialogManager.logsAndInputs.enumerated()), id: \.offset) { index, item in
-                                switch item {
-                                case .message(let dialog):
-                                    if dialog.sender == .user {
-                                        Text(dialog.content)
-                                            .font(NLPFont.body)
-                                            .foregroundStyle(.white)
-                                    } else {
-                                        StreamingText(fullDialog: dialog.content, streamingSpeed: 0.05)
-                                            .font(NLPFont.body)
-                                            .foregroundStyle(.white)
-                                    }
-                                case .input:
-                                    if !dialogManager.isGenerating {
-                                        ZStack(alignment: .leading) {
-                                            if inputText.isEmpty && !isFocused {
-                                                HStack(spacing: 0) {
-                                                    // 깜빡이는 커서
-                                                    if showCursor {
-                                                        Text("_") // 또는 "|"
-                                                            .foregroundColor(.gray)
-                                                            .font(NLPFont.body)
-                                                    }
-                                                }
-                                                .onReceive(cursorTimer) { _ in
-                                                    showCursor.toggle()
-                                                }
-                                            }
-                                            TextField("", text: $inputText, axis: .vertical)
-                                                .font(NLPFont.body)
-                                                .foregroundStyle(.white)
-                                                .focused($isFocused)
-                                                .submitLabel(.send)
-                                                .onSubmit {
-                                                    if !inputText.isEmpty {
-                                                        dialogManager.respond(inputText, dialogPartnerType: dialogManager.currentPartner ?? .computer, isLogged: true)
-                                                        inputText = ""
-                                                    }
-                                                }
-                                        }
-                                    }
-                                }
+        BackgroundView(isPresented: $isPresented)
+            .overlay(
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 10) {
+                        if let currentPartner = dialogManager.currentPartner, let conversationLogs = dialogManager.conversationLogs[currentPartner] {
+                            ForEach(conversationLogs, id: \.self) { log in
+                                Text(log.content)
+                                    .font(NLPFont.body)
+                                    .foregroundStyle(.white)
                             }
                         }
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        UserDialogInputField(
+                            inputText: $inputText,
+                            showCursor: $showCursor,
+                            isFocused: _isFocused,
+                            submitAction: {
+                                dialogManager.respond(
+                                    inputText,
+                                    dialogPartnerType: dialogManager.currentPartner ?? .computer,
+                                    isLogged: true
+                                )
+                                inputText = ""
+                            }
+                        )
+                        
+                        
                     }
-                    .frame(
-                        width: ConstantScreenSize.screenWidth * 0.82,
-                        height: ConstantScreenSize.screenHeight * 0.36
-                    )
+                    .onReceive(cursorTimer) { _ in
+                        showCursor.toggle()
+                    }
+                }
+                .frame(
+                    width: ConstantScreenSize.screenWidth * 0.82,
+                    height: ConstantScreenSize.screenHeight * 0.36
                 )
-        }
+            )
     }
 }
 
@@ -124,6 +104,39 @@ struct BackgroundView: View {
                     }
                 }
             }
+    }
+}
+
+struct UserDialogInputField: View {
+    @Binding var inputText: String
+    @Binding var showCursor: Bool
+    @FocusState var isFocused: Bool
+    let submitAction: () -> Void
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            if inputText.isEmpty && !isFocused {
+                HStack(spacing: 0) {
+                    // 깜빡이는 커서
+                    if showCursor {
+                        Text("_") // 또는 "|"
+                            .foregroundColor(.gray)
+                            .font(NLPFont.body)
+                    }
+                }
+            }
+            TextField("", text: $inputText, axis: .vertical)
+                .font(NLPFont.body)
+                .foregroundStyle(.white)
+                .focused($isFocused)
+                .submitLabel(.send)
+                .onSubmit {
+                    if !inputText.isEmpty {
+                        submitAction()
+                    }
+                }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
