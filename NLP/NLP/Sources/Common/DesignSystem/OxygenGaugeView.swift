e8e3f7324eecee3ce8472de2c6ecc9a4e4bc2b66
wonCha25 <chawj1234@gmail.com>
2025-07-22 20:44:56 +0900

    feat: oxygenGaugeview 구현

--- NLP/NLP/Sources/Common/DesignSystem/OxygenGaugeView.swift
+++ NLP/NLP/Sources/Common/DesignSystem/OxygenGaugeView.swift
@@ -8,61 +8,41 @@
 import SwiftUI
 
 struct OxygenGaugeView: View {
-    let initialOxygen: Int
+    @State private var oxygen: Int
     var coordinator: Coordinator
-    @State private var oxygen: Int = 0
-    @State private var timer: Timer? = nil
-    @State private var isBlackout: Bool = false
-    
+
+    init(initialOxygen: Int, coordinator: Coordinator) {
+        self._oxygen = State(initialValue: initialOxygen)
+        self.coordinator = coordinator
+    }
+
     var body: some View {
         let gaugeWidth = ConstantScreenSize.screenWidth * 0.78
         let gaugeHeight = ConstantScreenSize.screenHeight * 0.03
-        ZStack {
-            VStack {
-                ZStack(alignment: .leading) {
-                    Image("oxygenGauge")
-                        .resizable()
-                        .frame(width: gaugeWidth, height: gaugeHeight)
-                    Rectangle()
-                        .frame(width: gaugeWidth * CGFloat(oxygen) / 100, height: gaugeHeight)
-                        .foregroundColor(.green)
-                        .animation(.linear(duration: 0.3), value: oxygen)
-                        .padding(4)
-                    Text("산소: \(oxygen)%")
-                        .font(.custom("Galmuri9", size: gaugeHeight * 0.7))
-                        .foregroundColor(.white)
-                }
 
+        ZStack{
+            ZStack(alignment: .leading) {
+                Image("oxygenGauge")
+                    .resizable()
+                    .frame(width: gaugeWidth, height: gaugeHeight)
+                Rectangle()
+                    .frame(width: gaugeWidth * CGFloat(oxygen) / 100, height: gaugeHeight*0.7)
+                    .foregroundColor(.green)
+                    .animation(.linear(duration: 0.3), value: oxygen)
+                    .padding(4)
             }
-            if isBlackout {
-                Color.black.opacity(0.9)
-                    .ignoresSafeArea()
-                    .transition(.opacity)
-            }
+            Text("산소: \(oxygen)%")
+                .font(.custom("Galmuri9", size: gaugeHeight * 0.7))
+                .foregroundColor(.white)
         }
         .onAppear {
-            oxygen = initialOxygen
-            if timer == nil {
-                timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { t in
-                    if oxygen > 0 {
-                        oxygen -= 1
-                    } else {
-                        t.invalidate()
-                        timer = nil
-                        withAnimation {
-                            isBlackout = true
-                        }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                            coordinator.pop()
-                            coordinator.push(.stageOneScene)
-                        }
-                    }
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                if oxygen > 0 {
+                    oxygen -= 1
+                } else {
+                    timer.invalidate()
                 }
             }
-        }
-        .onDisappear {
-            timer?.invalidate()
-            timer = nil
         }
     }
 }
