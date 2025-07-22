7beda1b98b58c057457a297087c4d158869b1941
wonCha25 <chawj1234@gmail.com>
2025-07-22 23:41:43 +0900

    feat: 암전 효과 구현

--- NLP/NLP/Sources/Common/DesignSystem/OxygenGaugeView.swift
+++ NLP/NLP/Sources/Common/DesignSystem/OxygenGaugeView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct OxygenGaugeView: View {
     @State private var oxygen: Int
-    var coordinator: Coordinator
+    var onOxygenDepleted: () -> Void
 
-    init(initialOxygen: Int, coordinator: Coordinator) {
+    init(initialOxygen: Int, onOxygenDepleted: @escaping () -> Void) {
         self._oxygen = State(initialValue: initialOxygen)
-        self.coordinator = coordinator
+        self.onOxygenDepleted = onOxygenDepleted
     }
 
     var body: some View {
@@ -41,6 +41,7 @@ struct OxygenGaugeView: View {
                     oxygen -= 1
                 } else {
                     timer.invalidate()
+                    onOxygenDepleted()
                 }
             }
         }
@@ -50,6 +51,10 @@ struct OxygenGaugeView: View {
 #Preview {
     ZStack {
         Color.gray.ignoresSafeArea()
-        OxygenGaugeView(initialOxygen: 30, coordinator: Coordinator())
+        OxygenGaugeView(initialOxygen: 30) {
+            withAnimation(.linear(duration: 1)) {
+                viewModel.state.isTransitioning = true
+            }
+        }
     }
 }
