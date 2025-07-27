f27a8bccf67bd65df3d757d6f3e7d0a4cfd675a8
wonCha25 <chawj1234@gmail.com>
2025-07-27 16:38:20 +0900

    design: 스토리 asset 추가

--- NLP/NLP/Sources/Utils/Constants/ConstantMiddleStory.swift
+++ NLP/NLP/Sources/Utils/Constants/ConstantMiddleStory.swift
@@ -41,6 +41,8 @@ enum ConstantMiddleStories {
     static let stageTwoThree: [GameStory] = [
         // MARK: 임시
         GameStory(
+            storyImage: UIImage(named: "clock"),
+
             storyTitle: "시간의 비대칭성",
             storyDescription:
                 """
@@ -55,6 +57,7 @@ enum ConstantMiddleStories {
     
     static let stageThreeFour: [GameStory] = [
         GameStory(
+            storyImage: UIImage(named: "reunion"),
             storyTitle: "재회",
             storyDescription:
                 """
@@ -64,6 +67,8 @@ enum ConstantMiddleStories {
                 """
         ),
         GameStory(
+            storyImage: UIImage(named: "reunion"),
+
             storyTitle: "",
             storyDescription:
                 """
@@ -71,6 +76,7 @@ enum ConstantMiddleStories {
                 """
         ),
         GameStory(
+            storyImage: UIImage(named: "reunion"),
             storyTitle: "",
             storyDescription:
                 """
@@ -82,6 +88,8 @@ enum ConstantMiddleStories {
                 """
         ),
         GameStory(
+            storyImage: UIImage(named: "end_positive_1"),
+
             storyTitle: "사라진 고향",
             storyDescription:
                 """
@@ -96,17 +104,24 @@ enum ConstantMiddleStories {
     
     static let endingOne: [GameStory] = [
         GameStory(
+            storyImage: UIImage(named: "end_negative"),
+
             storyTitle: "끝",
             storyDescription: "우리는 지구의 멸망을 뒤로 하고, 키리듐 연구에 집중했다. 체력은 바닥났고, 남은 모든 연료를 다 소진하여 화성에 도달했다. 남은 희망은 키리듐을 채굴하여 이를 이용해 지구에 복귀하는 것.\n\n물론 남아있는 사람은 없을 것이다. 전부 죽었을 테니까. 하지만 이런 미래도 필요한 법이다. 수많은 미래가 존재하니까.\n\n새드 앤딩도 있어야 한다."
         ),
         GameStory(
+            storyImage: UIImage(named: "end_negative"),
+
             storyTitle: "",
             storyDescription: "우리의 선택이 누군가에게는 게임의 엔딩 중 하나일 뿐이다.\n\n선택의 기회가 다시 찾아온다면, 나는 어떤 결정을 내렸을까? 이 짧은 이야기의 결말은 달라졌을까?"
         )
     ]
     
     static let endingTwo: [GameStory] = [
-        GameStory(storyTitle: "끝", storyDescription: "엄청난 폭발음, 그리고 깨질듯한 머리.\n\n모두가 동시에 동일한 두통을 겪었다. 이틀 전부터의 기억이 이상하다. 모든 종류의 선택을 전부 경험한 것만 같았다.\n\n물리학자인 제인은 키리듐이 비활성화되며 모든 미래가 통합된 것 같다고 추측했다. 다양한 브랜치가 펼쳐지다가, 다시 하나가 된 것이라는 생각.")
+        GameStory(
+            storyImage: UIImage(named: "end_positive_0"),
+            storyTitle: "끝",
+            storyDescription: "엄청난 폭발음, 그리고 깨질듯한 머리.\n\n모두가 동시에 동일한 두통을 겪었다. 이틀 전부터의 기억이 이상하다. 모든 종류의 선택을 전부 경험한 것만 같았다.\n\n물리학자인 제인은 키리듐이 비활성화되며 모든 미래가 통합된 것 같다고 추측했다. 다양한 브랜치가 펼쳐지다가, 다시 하나가 된 것이라는 생각.")
     ]
 }
 
