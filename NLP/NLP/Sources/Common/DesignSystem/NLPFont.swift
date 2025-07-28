f054ab8346bf7554015a268b5bb43440beb7c2d7
Yang Sijun <yang@sijun.dev>
2025-07-28 11:51:25 +0900

    feat: NLPFont에sementic font system 추가

--- NLP/NLP/Sources/Common/DesignSystem/NLPFont.swift
+++ NLP/NLP/Sources/Common/DesignSystem/NLPFont.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 enum NLPFont {
-    static let body: Font = .custom("Galmuri11-Regular", size: 21)
+    static let body: Font = .custom("Galmuri11-Regular", size: 17)
+    static let gameOption: Font = .custom("Galmuri11-Regular", size: 17)
+    static let subtitle: Font = .custom("Galmuri11-Regular", size: 22)
+    static let chat: Font = .custom("Galmuri11-Regular", size: 16)
+    static let o2Gauge: Font = .custom("Galmuri9-Regular", size: 15)
+    static let password: Font = .custom("Galmuri9-Regular", size: 24)
+    static let itemInfoFrame: Font = .custom("Galmuri11-Regular", size: 14)
+    
     static let headline: Font = .custom("Galmuri11-Bold", size: 21)
 }
