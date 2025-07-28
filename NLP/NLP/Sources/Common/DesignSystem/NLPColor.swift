4d76711f82714bac7cae11c1e0e794236051fb5b
Yang Sijun <yang@sijun.dev>
2025-07-28 11:30:04 +0900

    feat: NLPColor의 sementic color system 추가

--- NLP/NLP/Sources/Common/DesignSystem/NLPColor.swift
+++ NLP/NLP/Sources/Common/DesignSystem/NLPColor.swift
@@ -10,4 +10,11 @@ import SwiftUI
 enum NLPColor {
     static let green = Color(red: 60/255, green: 255/255, blue: 0/255)
     static let gray1 = Color(red: 44/255, green: 44/255, blue: 44/255)
+    
+    static let primaryColor = Color(red: 60/255, green: 255/255, blue: 0/255)
+    static let errorColor = Color(red: 255/255, green: 0/255, blue: 0/255)
+    static let backgroundColor = Color(red: 30/255, green: 30/255, blue: 30/255, opacity: 0.5)
+    static let disableColor = Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.4)
+    static let gameOptionColor = Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.7)
+    static let fontColor = Color(red: 255/255, green: 255/255, blue: 255/255)
 }
